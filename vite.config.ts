55d172ead2ec77e1b145b832392642c6d598a11c
Alejandro Antonio <alejandrodelacruzcharun@gmail.com>
2024-09-20 17:19:51 -0500

    commit

--- vite.config.ts
+++ vite.config.ts
@@ -3,6 +3,5 @@ import react from '@vitejs/plugin-react-swc'
 
 // https://vitejs.dev/config/
 export default defineConfig({
-  plugins: [react()],
-  base: "http://www.inversionesmcgiversac.com/"
+  plugins: [react()]
     })
